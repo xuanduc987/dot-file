b0a93cf508a37b251602b268b00a8d2b187b9bab
Duc Nghiem Xuan <xuanduc987@gmail.com>
2016-02-17 23:10:17 +0700

    update dot.sh

--- dot.sh
+++ dot.sh
@@ -5,21 +5,29 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # vim
 ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim
+rm -r ~/.vim/ftplugin
+rm -r ~/.vim/snippets
+rm -r ~/.vim/UltiSnips
 ln -s ${BASEDIR}/vim/ftplugin ~/.vim/ftplugin
 ln -s ${BASEDIR}/vim/snippets ~/.vim/snippets
 ln -s ${BASEDIR}/vim/UltiSnips ~/.vim/UltiSnips
 
 # git
+rm ~/.gitconfig
+rm ~/.gitignore
 ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/git/gitignore ~/.gitignore
 
 # tmux
+rm ~/.tmux.conf
 ln -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
 
 # i3
+rm -r ~/.i3
 ln -s ${BASEDIR}/i3 ~/.i3
 
 # Xresources
+rm ~/.Xresources
 ln -s ${BASEDIR}/Xresources ~/.Xresources
 
 # mutt
