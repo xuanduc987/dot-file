5272c99a0da8143d4e2702ad36bf4d2e48aa09db
Duc Nghiem Xuan <git@ducnx.com>
2019-01-16 18:57:20 +0900

    zsh: update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,7 @@ alias ga='git add'
 alias gb='git branch'
 alias gc='git commit -v'
 alias gc!='git commit -v --amend'
+alias gcn!='git commit -v --no-edit --amend'
 alias gca='git commit -v -a'
 alias gca!='git commit -v -a --amend'
 alias gcan!='git commit -v -a --no-edit --amend'
@@ -15,18 +16,20 @@ alias gdca='git diff --cached'
 alias gf='git fetch'
 alias gfo='git fetch origin'
 alias gignore='git update-index --assume-unchanged'
-alias gl='git pull'
 alias glg='git log --stat'
 alias gp='git push'
 alias gr='git remote'
-alias gst='git status'
 alias gsta='git stash save'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
+alias gs='git status'
+alias grs='git reset'
+alias grb='git rebase'
+alias gm='git merge'
+alias gll='git pull'
 
 ###
 
 alias ls="ls --color=tty"
-alias :q="exit"
 
 alias zshrc="vim ~/.zshrc"
 alias vimrc="vim ~/.vimrc"
