adc1ac881ba79c481c5e6024ce2c43c46657641a
Duc Nghiem-Xuan <git@ducnx.com>
2020-02-12 15:36:39 +0900

    fix shell check

--- dot.sh
+++ dot.sh
@@ -4,36 +4,34 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
 rm ~/.vimrc
-ln -s ${BASEDIR}/vim/vimrc ~/.vimrc
+ln -s "${BASEDIR}/vim/vimrc" ~/.vimrc
 mkdir -p ~/.vim
 rm -r ~/.vim/ftplugin
-rm -r ~/.vim/snippets
 rm -r ~/.vim/UltiSnips
-ln -s ${BASEDIR}/vim/ftplugin ~/.vim/ftplugin
-ln -s ${BASEDIR}/vim/snippets ~/.vim/snippets
-ln -s ${BASEDIR}/vim/UltiSnips ~/.vim/UltiSnips
-cp ${BASEDIR}/vim/autoload/*.* ~/.vim/autoload/
-cp ${BASEDIR}/vim/vimrc.lang ~/.vimrc.lang
-cp ${BASEDIR}/vim/vimrc.lang.plug ~/.vimrc.lang.plug
+ln -s "${BASEDIR}/vim/ftplugin" ~/.vim/ftplugin
+ln -s "${BASEDIR}/vim/UltiSnips" ~/.vim/UltiSnips
+cp "${BASEDIR}/vim/autoload/*.*" ~/.vim/autoload/
+cp "${BASEDIR}/vim/vimrc.lang" ~/.vimrc.lang
+cp "${BASEDIR}/vim/vimrc.lang.plug" ~/.vimrc.lang.plug
 
 # git
 rm ~/.gitconfig
 rm ~/.gitignore
-ln -s ${BASEDIR}/git/gitconfig ~/.gitconfig
-ln -s ${BASEDIR}/git/gitignore ~/.gitignore
+ln -s "${BASEDIR}/git/gitconfig" ~/.gitconfig
+ln -s "${BASEDIR}/git/gitignore" ~/.gitignore
 
 # tmux
 rm ~/.tmux.conf
-ln -s ${BASEDIR}/tmux/tmux.conf ~/.tmux.conf
+ln -s "${BASEDIR}/tmux/tmux.conf" ~/.tmux.conf
 
 # i3
 rm -r ~/.i3
-ln -s ${BASEDIR}/i3 ~/.i3
+ln -s "${BASEDIR}/i3" ~/.i3
 
 # Xresources
 rm ~/.Xresources
-ln -s ${BASEDIR}/Xresources ~/.Xresources
+ln -s "${BASEDIR}/Xresources" ~/.Xresources
 
 # zsh
 SOURCE_LINE="source ${BASEDIR}/zsh/zshrc"
-grep -q "$SOURCE_LINE" ~/.zshrc || echo "$SOURCE_LINE" >> ~/.zshrc
+grep -q "${SOURCE_LINE}" ~/.zshrc || echo "${SOURCE_LINE}" >> ~/.zshrc
